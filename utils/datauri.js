a47d74ec0ea17d51aa50d7a51d306e499d101cf2
Prabhdeep24 <prabhdeeprandhawa16369@gmail.com>
2024-08-18 20:23:09 +0530

    error handling changes

--- utils/datauri.js
+++ utils/datauri.js
@@ -4,6 +4,7 @@ import path from "path";
 const getDataUri = (file) => {
     const parser = new DataUriParser();
     const extName = path.extname(file.originalname).toString();
+    console.log(`ext name --- ${extName},  PARSER ---- ${file}`);
     return parser.format(extName,file.buffer);
 }
 
