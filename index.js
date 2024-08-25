d9f568253a311df2438a6f88c64e4d0e156e0d13
Prabhdeep singh <47300462+Prabhdeep24@users.noreply.github.com>
2024-08-25 16:59:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://job-portal-react-two.vercel.app/",
+  origin: "https://66cb14ae882f3dfbd3c736d3--incredible-platypus-0bea99.netlify.app/",
   credentials: true,
 };
 app.use(cors(corsOptions));
